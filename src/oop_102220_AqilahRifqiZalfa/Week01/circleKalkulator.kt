b561408027ac61b1e5df65796677a36b88b768d5
Aqilah1211 <email@anda.com>
2026-03-09 23:22:09 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_102220_AqilahRifqiZalfa/Week01/circleKalkulator.kt
+++ src/oop_102220_AqilahRifqiZalfa/Week01/circleKalkulator.kt
@@ -2,11 +2,13 @@ package oop_00000102220_AqilahRifqiZalfa.week01
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) 20 else 10
 
-// Fungsi printReceipt ✨
-fun printReceipt(title: String, finalPrice: Int) {
+// Fungsi printReceipt dengan parameter note (nullable + default value) ✨
+fun printReceipt(title: String, finalPrice: Int, note: String? = null) {
     println("=== STRUK PEMBELIAN ===")
     println("Judul Game: $title")
     println("Harga Akhir: Rp $finalPrice")
+    // Elvis Operator untuk null safety ✨
+    println("Catatan: ${note ?: "Tidak ada catatan"}")
     println("=======================")
 }
 
@@ -14,10 +16,13 @@ fun main() {
     val gameTitle = "Cyber Adventure"
     val price = 750000
 
+    // Nullable variable ✨
+    val userNote: String? = null
+
     val discountPercent = calculateDiscount(price)
     val discountAmount = price * discountPercent / 100
     val finalPrice = price - discountAmount
 
-    // Named Arguments ✨ (WAJIB!)
-    printReceipt(title = gameTitle, finalPrice = finalPrice)
+    // Named Arguments + nullable note ✨
+    printReceipt(title = gameTitle, finalPrice = finalPrice, note = userNote)
 }
