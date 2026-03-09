c1e646dd9fd18f1e49740bce27453ec104b2f92f
Aqilah1211 <email@anda.com>
2026-03-09 23:19:27 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/oop_102220_AqilahRifqiZalfa/Week01/circleKalkulator.kt
+++ src/oop_102220_AqilahRifqiZalfa/Week01/circleKalkulator.kt
@@ -1,15 +1,13 @@
 package oop_00000102220_AqilahRifqiZalfa.week01
 
-// Ubah fungsi menjadi Expression Body & mengembalikan String ✨
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
 
-fun main(args: Array<String>) {
+// Hapus parameter args yang tidak dipakai ✨
+fun main() {
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
-
-    // Panggil fungsi di dalam println ✨
     println(checkSize(area))
 }
