8b247dde8631e24323cad5f2c01d90dff0a29a21
Aqilah1211 <email@anda.com>
2026-03-09 23:20:24 +0700

    week01: (tugas 2) setup GameStore file

--- src/oop_102220_AqilahRifqiZalfa/Week01/circleKalkulator.kt
+++ src/oop_102220_AqilahRifqiZalfa/Week01/circleKalkulator.kt
@@ -1,13 +1,7 @@
 package oop_00000102220_AqilahRifqiZalfa.week01
 
-fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
-
-// Hapus parameter args yang tidak dipakai ✨
 fun main() {
-    val radius = 7.0
-    val pi = 3.14
-    val area = pi * radius * radius
-
-    println("Radius: $radius, Area: $area")
-    println(checkSize(area))
+    // Data dengan val ✨
+    val gameTitle = "Cyber Adventure"
+    val price = 750000
 }
