f0c087b36ac3a1b239efc61c4fa9890b7df63b91
Aqilah1211 <email@anda.com>
2026-03-09 23:12:11 +0700

    week01: implement grading logic using when expression

--- src/oop_102220_AqilahRifqiZalfa/Week01/GradingSystem.kt
+++ src/oop_102220_AqilahRifqiZalfa/Week01/GradingSystem.kt
@@ -1,23 +1,17 @@
 package oop_00000102220_AqilahRifqiZalfa.week01
 
 fun main() {
-    // Ubah var menjadi val (immutable)
     val studentName = "John Doe"
     val score = 85.5
-    var grade = ""  // grade tetap var karena akan diubah
 
-    if (score >= 90) {
-        grade = "A"
-    } else if (score >= 80) {
-        grade = "B"
-    } else if (score >= 70) {
-        grade = "C"
-    } else if (score >= 60) {
-        grade = "D"
-    } else {
-        grade = "E"
+    // when sebagai Expression (mengembalikan nilai langsung) ✨
+    val grade = when {
+        score >= 90 -> "A"
+        score >= 80 -> "B"
+        score >= 70 -> "C"
+        score >= 60 -> "D"
+        else -> "E"
     }
 
-    // Gunakan String Template ($) instead of (+)
     println("Student: $studentName, Score: $score, Grade: $grade")
 }
