0dbcb84f424a886840a9c97a7d43d252f5ce6e88
Aqilah1211 <email@anda.com>
2026-03-09 23:21:01 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_102220_AqilahRifqiZalfa/Week01/circleKalkulator.kt
+++ src/oop_102220_AqilahRifqiZalfa/Week01/circleKalkulator.kt
@@ -1,7 +1,13 @@
 package oop_00000102220_AqilahRifqiZalfa.week01
 
+// Fungsi diskon dengan Expression Body ✨
+fun calculateDiscount(price: Int): Int = if (price > 500000) 20 else 10
+
 fun main() {
-    // Data dengan val ✨
     val gameTitle = "Cyber Adventure"
     val price = 750000
+
+    val discountPercent = calculateDiscount(price)
+    val discountAmount = price * discountPercent / 100
+    val finalPrice = price - discountAmount
 }
