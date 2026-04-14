0881a1e0c0c5f1450a751d8fbbb58a498246d700
Aqilah1211 <email@anda.com>
2026-04-14 15:31:25 +0700

    week08: create nested classes for Order

---
+++ src/oop_102220_AqilahRifqiZalfa/Week 8/OrderModels.kt
@@ -0,0 +1,7 @@
+package oop_102220_AqilahRifqiZalfa.`Week 8`
+
+package oop.nim.nama.week08
+
+data class Address(val city: String?)
+data class Customer(val address: Address?)
+data class Order(val customer: Customer?)
