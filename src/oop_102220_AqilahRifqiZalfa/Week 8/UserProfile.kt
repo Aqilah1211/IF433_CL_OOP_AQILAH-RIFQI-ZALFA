544175870c599f3f33afec24d86e635cb0e50208
Aqilah1211 <email@anda.com>
2026-04-14 15:29:03 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_102220_AqilahRifqiZalfa/Week 8/UserProfile.kt
@@ -0,0 +1,10 @@
+package oop_102220_AqilahRifqiZalfa.`Week 8`
+
+package oop.nim.nama.week08
+
+data class UserProfile(
+    val id: String,
+    val name: String,
+    val email: String?,
+    val phone: String?
+)
