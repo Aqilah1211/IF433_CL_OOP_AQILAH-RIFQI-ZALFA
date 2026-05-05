61c385a9694bf9b5cf69d2ea792e28a900f7856e
Aqilah1211 <email@anda.com>
2026-05-05 15:44:42 +0700

    week11: test extension functions in main

---
+++ src/oop_102220_AqilahRifqiZalfa/Week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_102220_AqilahRifqiZalfa.Week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
