a8f5811228a1b9f30a0f3aa909ae8315e26209d2
Aqilah1211 <email@anda.com>
2026-05-05 15:23:32 +0700

    week11: implement let function for null safety

---
+++ src/oop_102220_AqilahRifqiZalfa/Week11/ScopeLetRun.kt
@@ -0,0 +1,19 @@
+package oop_102220_AqilahRifqiZalfa.Week11
+
+package oop_102220_Aqil.week11
+
+fun main() {
+    val text: String? = "hello"
+
+    // LET
+    text?.let {
+        println("Length: ${it.length}")
+    }
+
+    // RUN
+    val result = text?.run {
+        length * 2
+    }
+
+    println("Result: $result")
+}
