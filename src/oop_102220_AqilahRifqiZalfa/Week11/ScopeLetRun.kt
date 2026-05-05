919ab65d6cfaa85146a3dd507917ec47db25f0c5
Aqilah1211 <email@anda.com>
2026-05-05 15:46:06 +0700

    week11: implement run function for object computation

--- src/oop_102220_AqilahRifqiZalfa/Week11/ScopeLetRun.kt
+++ src/oop_102220_AqilahRifqiZalfa/Week11/ScopeLetRun.kt
@@ -10,3 +10,9 @@ fun main() {
         it.length // Mengembalikan nilai expression terakhir
     }
     println("Panjang nama: $length")
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}") // 'this' merujuk pada "Kotlin"
+        this.length * 2
+    }
+    println("Hasil kalkulasi run: $result")
