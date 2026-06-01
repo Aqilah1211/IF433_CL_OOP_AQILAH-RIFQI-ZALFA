f2616c7796e230194a35f4e8dac3e95b82974c13
Aqilah1211 <email@anda.com>
2026-06-02 04:20:52 +0700

    week02: create basic Student class structure

---
+++ src/oop_102220_AqilahRifqiZalfa/Week2/Student.kt
@@ -0,0 +1,8 @@
+package oop_102220_AqilahRifqiZalfa.Week2
+
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String = "Non-Matriculated",
+    var gpa: Double = 0.0
+) {
