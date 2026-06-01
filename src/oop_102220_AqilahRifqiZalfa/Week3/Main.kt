0630b7bc20df972204aa0e28f69835b99a3891fa
Aqilah1211 <email@anda.com>
2026-06-02 04:32:44 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_102220_AqilahRifqiZalfa/Week3/Main.kt
+++ src/oop_102220_AqilahRifqiZalfa/Week3/Main.kt
@@ -1,5 +1,26 @@
 package oop_102220_AqilahRifqiZalfa.Week3
 
+class Employee(val name: String) {
+    var salary: Long = 0
+        set(value) {
+            if (value < 0) {
+                println("WARNING: Gaji tidak boleh negatif!")
+            } else {
+                field = value
+            }
+        }
+
+    private var performanceScore: Int = 0
+
+    fun increasePerformance() {
+        performanceScore += 10
+        println("$name sedang meningkatkan performa!")
+    }
+
+    val tax: Double
+        get() = salary * 0.1
+}
+
 fun main() {
     val e = Employee("Budi")
 
