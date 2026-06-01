53f777411bead40a3dc3e32ccc2dabaa09c61ca0
Aqilah1211 <email@anda.com>
2026-06-02 04:31:27 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_102220_AqilahRifqiZalfa/Week3/Player.kt
@@ -0,0 +1,18 @@
+package oop_102220_AqilahRifqiZalfa.Week3
+
+class Player(val username: String) {
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount > 0) {
+            val previousLevel = level
+            xp += amount
+            if (level > previousLevel) {
+                println("Level Up! Selamat $username naik ke level $level")
+            }
+        }
+    }
+}
