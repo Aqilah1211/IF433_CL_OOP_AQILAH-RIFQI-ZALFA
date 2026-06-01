9c3ebd4709aa981e3d639e534d0d90a4581595dd
Aqilah1211 <email@anda.com>
2026-06-02 04:29:09 +0700

    week03: test all encapsulation logic in main

---
+++ src/oop_102220_AqilahRifqiZalfa/Week3/Main.kt
@@ -0,0 +1,12 @@
+package oop_102220_AqilahRifqiZalfa.Week3
+
+fun main() {
+    val e = Employee("Budi")
+
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+    println("Pajak yang harus dibayar: ${e.tax}")
+}
