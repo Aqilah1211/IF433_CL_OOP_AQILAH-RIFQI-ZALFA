4a37878d1c6aa01e69325e574f955aa3cfe0bf4c
Aqilah1211 <email@anda.com>
2026-06-02 04:21:09 +0700

    week02: add warning validation in init block

--- src/oop_102220_AqilahRifqiZalfa/Week2/Student.kt
+++ src/oop_102220_AqilahRifqiZalfa/Week2/Student.kt
@@ -6,3 +6,12 @@ class Student (
     var major: String = "Non-Matriculated",
     var gpa: Double = 0.0
 ) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
+}
